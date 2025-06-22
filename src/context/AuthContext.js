c9b9c8bb9fa3f973e738a5a4dafa92151524223e
Harshith14hs <harshithhs14@gmail.com>
2025-06-22 18:19:29 +0530

    sec

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,7 +5,7 @@ const AuthContext = createContext(null);
 
 export const useAuth = () => useContext(AuthContext);
 
-const API_BASE_URL =  'https://prm-back-harshith-hs-projects.vercel.app/';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://pm-back.onrender.com';
 
 
 export const AuthProvider = ({ children, onSessionExpired }) => {
